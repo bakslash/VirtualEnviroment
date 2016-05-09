b555e30e2c431d945022e8564d5b0beedf093589
bakslash <marcusmuasa@gmail.com>
2016-05-10 00:53:02 +0300

    templates'

--- app/views.py
+++ app/views.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python
-
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    user = {'nickname':'Marcus'}
-    return '''
-<html>
-   <head>
-    <title>Home Page</title>
-    </head>
-    <body>
-    <h1>Hello, ''' + user['nickname'] +'''</h1>
-   </body>
-  </html>
-'''  
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
