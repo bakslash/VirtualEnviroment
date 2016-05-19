87726048ca018d78f4d65093aeff2d2d71f6bb8a
bakslash <marcusmuasa@gmail.com>
2016-05-20 02:39:01 +0300

    debug

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,4 @@
-#!/usr/bin/python
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object('config')
-
 from app import views
-
-
--- app/views.py
+++ app/views.py
@@ -1,14 +1,20 @@
-#!/usr/bin/python
-from flask import render_template, flash, redirect
+from flask import render_template
 from app import app
-from .forms import LoginForm
-
-#index view function suppresed for brevity
-@app.route('/login', methods=['GET','POST'])
 
 
-
-def login():
-    form = LoginForm()
-    return render_template('login.html',title='Sign In',form=form)
-
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname':'Miguel'} # fake user
+    posts = [# fake array of posts
+        {
+          'author': {'nickname':'john'},
+          'body': 'Beautiful day in portland'
+        },
+        {
+           'author': {'nickname':'susan'},  
+           'body': 'The Avengers movies was so cool!'
+        }  
+   ]
+    return render_template("index.html",title='Home',user=user,posts=posts)
+ 
